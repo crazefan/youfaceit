aba3e8e7bf592ad66f9503f08affd5437af17731
crazefan <dolgopolovt@gmail.com>
2021-04-05 03:07:42 +0200

    added api requests for player's data and history, refactures for DRY

--- src/api/index.js
+++ src/api/index.js
@@ -1,48 +1,18 @@
 import axios from "axios";
 import { faceitApiKey } from "../config.js";
 
-export const fetchMatchData = (matchId) => {
-  return axios
-    .get(`https://open.faceit.com/data/v4/matches/${matchId}/stats`, {
-      headers: {
-        Authorization: "Bearer " + faceitApiKey,
-      },
-    })
-    .then(({ data }) => {
-      return data;
-    })
-    .catch((err) => {
-      console.error(err);
-      Promise.reject(err);
-    });
-};
+const faceitApi = axios.create({
+  headers: {
+    Authorization: "Bearer " + faceitApiKey,
+  },
+  baseURL: "https://open.faceit.com/data/v4/",
+});
 
-export const fetchPlayerData = (nickname) => {
-  return axios
-    .get(`https://open.faceit.com/data/v4/players?nickname=${nickname}`, {
-      headers: {
-        Authorization: "Bearer " + faceitApiKey,
-      },
-    })
-    .then(({ data }) => {
-      return data;
-    })
-    .catch((err) => {
-      console.error(err);
-      Promise.reject(err);
-    });
-};
+faceitApi.interceptors.response.use(
+  (response) => response.data,
+  (error) => Promise.reject(error)
+);
 
-// const faceitApi = axios.create({
-//   headers: {
-//     Authorization: "Bearer " + faceitApiKey,
-//   },
-//   baseURL: "https://open.faceit.com/data/v4/",
-// });
-
-// faceitApi.interceptors.response.use(
-//   (response) => response.data,
-//   (error) => Promise.reject(error)
-// );
-
-// export const fetchMatchData = (matchId) => faceitApi.get(`matches/${matchId}/stats`);
+export const fetchMatchData = (matchId) => faceitApi.get(`matches/${matchId}/stats`);
+export const fetchPlayerData = (nickname) => faceitApi.get(`players?nickname=${nickname}`);
+export const fetchPlayerHistory = (playerId) => faceitApi.get(`players/${playerId}/history`);
