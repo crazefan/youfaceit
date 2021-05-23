cd6240246e26fe1981de52115ea7f6758378463d
crazefan <dolgopolovt@gmail.com>
2021-05-23 22:29:13 +0200

    message refactored

--- src/utils/message.js
+++ src/utils/message.js
@@ -1,45 +1,15 @@
 import { multiLanguageEmbedText } from "./multiLanguageText.js";
 
-const formEmbeddedMessage = (messageData) => {
-  return {
-    color: 0xde9012,
-    author: {
-      name: "YouFaceIt",
-      icon_url: "https://i.imgur.com/Kovfkbl.png",
-      url: "https://github.com/crazefan/faceitloser",
-    },
-    thumbnail: {
-      url: messageData.messageThumbnail,
-    },
-    fields: [
-      {
-        name: `${messageData.names.gameSummary}\n\u200b`,
-        value: `${messageData.body}\n\u200b`,
-      },
-
-      {
-        name: messageData.names.mapInfo,
-        value: messageData.mapData,
-        inline: true,
-      },
-      {
-        name: messageData.names.scoreboard,
-        value: `\`${messageData.scoreboard}\``,
-        inline: true,
-      },
-    ],
-  };
-};
-
 export const composeMessage = (
   matchData,
   hasWon,
-  teamTable,
+  teamScoreboard,
   bestPerformedPlayer,
   leastPerfomedPlayer,
   serverLanguage
 ) => {
   const text = serverLanguage === "en" ? multiLanguageEmbedText().en : multiLanguageEmbedText().ru;
+
   const isVictory = hasWon ? text.isVictory.win : text.isVictory.lose;
 
   const messageThumbnail = hasWon
@@ -49,18 +19,34 @@ export const composeMessage = (
   const bestPerformer = `${text.bestPerformer.begin} - **${bestPerformedPlayer.nickname}**! ${text.bestPerformer.end}`;
   const worstPerformer = `${text.worstPerformer.begin} **${leastPerfomedPlayer.nickname}**. ${text.worstPerformer.end}`;
 
-  const mapInfo = [
-    `${text.mapInfo.map} ${matchData.rounds[0].round_stats.Map}`,
-    `${text.mapInfo.score} ${matchData.rounds[0].round_stats.Score}`,
-  ];
+  const mapInfo = `${text.mapInfo.map} ${matchData.rounds[0].round_stats.Map}\n${text.mapInfo.score} ${matchData.rounds[0].round_stats.Score}`;
 
-  const messageData = {
-    body: `${isVictory}\n\n${bestPerformer}\n${worstPerformer}`,
-    mapData: mapInfo.join("\n"),
-    scoreboard: teamTable,
-    names: text.names,
-    messageThumbnail: messageThumbnail,
+  return {
+    color: 0xde9012,
+    author: {
+      name: "YouFaceIt",
+      icon_url: "https://i.imgur.com/Kovfkbl.png",
+      url: "https://github.com/crazefan/faceitloser",
+    },
+    thumbnail: {
+      url: messageThumbnail,
+    },
+    fields: [
+      {
+        name: `${text.names.gameSummary}\n\u200b`,
+        value: `${isVictory}\n\n${bestPerformer}\n${worstPerformer}\n\u200b`,
+      },
+
+      {
+        name: text.names.mapInfo,
+        value: mapInfo,
+        inline: true,
+      },
+      {
+        name: text.names.scoreboard,
+        value: `\`${teamScoreboard}\``,
+        inline: true,
+      },
+    ],
   };
-
-  return formEmbeddedMessage(messageData);
 };
